64f1a76e9cab21b235b64850e4080f57977de53d
AD9000 <damle.atharv16@gmail.com>
2020-08-16 18:17:33 +1000

    Updates

---
+++ precache-manifest.73dcb32405c96561ae1f1312ec39c5b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0077a93f3b8ce2d0afb9ec8d954af5",
+    "url": "/dvorak.atharvdamle.com/index.html"
+  },
+  {
+    "revision": "dc7d4f405b99533c06a2",
+    "url": "/dvorak.atharvdamle.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cac764bce0d883a83114",
+    "url": "/dvorak.atharvdamle.com/static/js/2.de6767fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dvorak.atharvdamle.com/static/js/2.de6767fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7d4f405b99533c06a2",
+    "url": "/dvorak.atharvdamle.com/static/js/main.5f4e5e43.chunk.js"
+  },
+  {
+    "revision": "05b952327fdcfba6e976",
+    "url": "/dvorak.atharvdamle.com/static/js/runtime-main.50c4751e.js"
+  }
+]);
